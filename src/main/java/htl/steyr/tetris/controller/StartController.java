8145341bbd7ad7f4ec2f36fe170339ca6365b761
Clemens Seiler <cseiler@htl-steyr.ac.at>
2026-05-20 19:58:35 +0200

    Grundstrukur des Startbildschirms nach unserer Skizze erstellt

---
+++ src/main/java/htl/steyr/tetris/controller/StartController.java
@@ -0,0 +1,16 @@
+package htl.steyr.tetris.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class StartController {
+
+    @FXML private Button closeButton;
+    @FXML private Button okButton;
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+
+
+}
