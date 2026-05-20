726e48389b788679c869094f6c6a28c8f61cfe49
samthurner <sthurner@htl-steyr.ac.at>
2026-05-20 14:12:56 +0200

    Umfassende UserData Klasse kreiert. Mit hilfsmitteln wie Gson und google.common.hash. bei neuen Usern werden direkt default settings angewendet.

--- src/main/java/htl/steyr/tetris/user/UserSession.java
+++ src/main/java/htl/steyr/tetris/user/UserSession.java
@@ -1,4 +1,13 @@
 package htl.steyr.tetris.user;
 
 public class UserSession {
+
+    private static UserData ud = null;
+
+    public static void setUserData(UserData userData){
+        ud = userData;
+    }
+    public static UserData getUserData(){
+        return ud;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module htl.steyr.tetris {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.common;
+    requires com.google.gson;
 
 
     opens htl.steyr.tetris to javafx.fxml;
