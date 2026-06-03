c6fb97d9e9e402eec12fcea25ba7d8f330e41dfc
Clemens Seiler <cseiler@htl-steyr.ac.at>
2026-06-03 14:55:18 +0200

    Application so umgeändert das beim starten der Whitemode geladen wird

--- src/main/java/htl/steyr/tetris/TetrisApplication.java
+++ src/main/java/htl/steyr/tetris/TetrisApplication.java
@@ -11,9 +11,12 @@ import java.io.IOException;
 public class TetrisApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TetrisApplication.class.getResource("menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TetrisApplication.class.getResource("options.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
+        scene.getStylesheets().add(
+                getClass().getResource("/stylesheets/whitemode.css").toExternalForm()
+        );
         ViewSwitcher.setDarkMode();
         stage.setScene(scene);
         stage.show();
