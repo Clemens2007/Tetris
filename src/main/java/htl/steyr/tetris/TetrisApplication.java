1d5686e5efd3d74f7b43287bc611d544eee43bf8
samue <sthurner@htl-steyr.ac.at>
2026-06-21 17:57:50 +0200

    Einheitsgröße + Undecorated sieht am besten aus, fullscreen war verbugged.

--- src/main/java/htl/steyr/tetris/TetrisApplication.java
+++ src/main/java/htl/steyr/tetris/TetrisApplication.java
@@ -7,6 +7,11 @@ import javafx.stage.Stage;
 public class TetrisApplication extends Application {
     @Override
     public void start(Stage stage) {
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setWidth(900);
+        stage.setHeight(700);
+        stage.setResizable(false);
+
         stage.setTitle("Tetris");
         ViewSwitcher.setStage(stage);
         ViewSwitcher.switchTo("start.fxml");
